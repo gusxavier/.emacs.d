cd1b68e30ad57d29d5da25477eea93e14ee4b827
Gustavo Xavier <gustavoxavier94@icloud.com>
2018-03-30 13:47:18 -0300

    remove gometalinter (for now)

--- customization/go.el
+++ customization/go.el
@@ -11,12 +11,6 @@
               (set (make-local-variable 'company-backends) '(company-go))
               (company-mode))))
 
-(use-package flycheck-gometalinter
-  :ensure t
-  :config
-  (progn
-    (flycheck-gometalinter-setup)))
-
 (use-package go-mode
   :config
   (setq gofmt-command "goimports")
