21f47a642ffff1372d6c11844e7b50a244c9d900
Gustavo Xavier <gustavoxavier94@icloud.com>
2019-05-28 15:16:37 -0300

    Remove meghanada auto format

--- customization/java.el
+++ customization/java.el
@@ -12,9 +12,7 @@
               ;; meghanada-mode on
               (meghanada-mode t)
               (flycheck-mode +1)
-              (setq c-basic-offset 2)
-              ;; use code format
-              (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+              (setq c-basic-offset 2)))
   (cond
    ((eq system-type 'windows-nt)
     (setq meghanada-java-path (expand-file-name "bin/java.exe" (getenv "JAVA_HOME")))
