4470bc37106d399c0e2fc82b5fd5f602fd2c7fd8
Gustavo Xavier <gustavoxavier94@icloud.com>
2019-06-27 18:58:46 -0300

    Remove fixed web mode indentation

--- customization/web.el
+++ customization/web.el
@@ -8,11 +8,6 @@
   :config
   (defun my-web-mode-hook ()
     "Hooks for web mode"
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)
-    (setq web-mode-style-padding 2)
-    (setq web-mode-script-padding 2)
     (setq web-mode-enable-auto-pairing t)
     (setq web-mode-enable-current-element-highlight t)
     (setq web-mode-enable-auto-indentation nil)
