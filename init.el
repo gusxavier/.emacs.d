0af1fc8ee8b9315c6b33935e59b7231f6e844f74
Gustavo Xavier <gusxavier@protonmail.com>
2022-07-10 00:38:26 -0300

    Add org-roam + UI improvements

--- init.el
+++ init.el
@@ -61,28 +61,6 @@
 (setq auto-save-file-name-transforms
       `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
 
-;; Reload buffers on disk change
-(global-auto-revert-mode t)
-
-;; Just type y or n instead of yes or no
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; Avoid generating auto save files
-;; [TODO]: use no-littering package
-;; (setq auto-save-default nil)
-;; (setq create-lockfiles nil)
-;; (setq make-backup-files nil)
-
-;; Set command as meta key in mac
-(when (eq system-type 'darwin)
-  (setq mac-option-key-is-meta nil
-        mac-command-key-is-meta t
-        mac-command-modifier 'meta
-        mac-option-modifier 'none))
-
-;; Ask before exit
-(setq confirm-kill-emacs 'y-or-n-p)
-
 ;; Better comments
 (use-package evil-nerd-commenter
   :defer t
@@ -112,13 +90,40 @@
   :config
   (which-key-mode +1))
 
+;; Reload buffers on disk change
+(global-auto-revert-mode t)
+
+;; Just type y or n instead of yes or no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Set command as meta key in mac
+(when (eq system-type 'darwin)
+  (setq mac-option-key-is-meta nil
+        mac-command-key-is-meta t
+        mac-command-modifier 'meta
+        mac-option-modifier 'none))
+
+;; Ask before exit
+(setq confirm-kill-emacs 'y-or-n-p)
+
+(defvar my/config-file
+  (expand-file-name "Emacs.org" user-emacs-directory))
+
+(defun my/load-config-file ()
+  (interactive)
+  (switch-to-buffer (find-file-noselect my/config-file)))
+
+;; Keybinding to open config file
+(global-set-key (kbd "<f9>") 'my/load-config-file)
+
 (use-package corfu
   :after orderless
   :init
   (global-corfu-mode)
   :config
   (setq corfu-auto t)
-  (setq corfu-cycle t))
+  (setq corfu-cycle t)
+  (setq corfu-auto-prefix 0))
 
 ;; Better completion style
 (use-package orderless
@@ -195,79 +200,21 @@
 (global-set-key (kbd "C-c p p") 'project-switch-project)
 (global-set-key (kbd "C-c p f") 'project-find-file)
 
-(defun my/org-mode-setup ()
-  "Custom 'org-mode' setup."
-  (org-indent-mode)
-  (visual-line-mode 1))
-
-(use-package org
-  :pin org
-
-  :commands (org-capute org-agenda)
-
-  :hook
-  (org-mode . my/org-mode-setup)
-
-  :config
-  (setq org-ellipsis " ▾")
-  (setq org-hide-emphasis-markers t)
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "NEXT(n)" "REVIEW(r)" "|" "DONE(d!)")))
-
-  (setq org-agenda-start-with-log-mode t)
-
-  (setq org-log-done 'time)
-
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-  (require 'org-tempo)
-  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-
-  (org-babel-do-load-languages 'org-babel-load-languages
-                               '((emacs-lisp . t)))
-
-  (setq org-confirm-babel-evaluate nil)
-
-  (defun my/org-babel-tangle-config ()
-    (when (string-equal (buffer-file-name)
-                        (expand-file-name "Emacs.org" user-emacs-directory))
-      ;; Dynamic scoping to the rescue
-      (let ((org-confirm-babel-evaluate nil))
-        (org-babel-tangle))))
-
-  ;; Automatically tangle our Emacs.org config file when we save it
-  (add-hook 'org-mode-hook
-            (lambda ()
-              (add-hook 'after-save-hook #'my/org-babel-tangle-config))))
-
-(use-package org-bullets
-  :after org
-  :hook
-  (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
 (defvar my/default-font "PragmataPro Liga")
 
 ;; Set a different font size between MacOS and Linux
-(defvar my/default-font-height (if (eq system-type 'darwin) 220 190))
+(defvar my/default-font-height (if (eq system-type 'darwin) 220 160))
 
 (set-face-attribute 'default nil
                     :family my/default-font
                     :height my/default-font-height
                     :weight 'regular)
 
-;; Function to set the mode-line font a little smaller
-;; Will be used in the configuration below
-(defun my/set-modeline-font-size ()
-  (set-face-attribute 'mode-line nil
-                      :height (- my/default-font-height 30))
-  (set-face-attribute 'mode-line-inactive nil
-                      :height (- my/default-font-height 30)))
+;; Fixed pitch font is used in places like org mode src blocks
+(set-face-attribute 'fixed-pitch nil
+                    :family my/default-font
+                    :height my/default-font-height
+                    :weight 'regular)
 
 ;; Set encoding to UTF-8
 (set-language-environment "UTF-8")
@@ -281,8 +228,8 @@
 (pragmatapro-lig-global-mode)
 
 ;; Run M-x all-the-icons-install-fonts in the first time
-(use-package all-the-icons
-  :if (display-graphic-p))
+;; (use-package all-the-icons
+;;   :if (display-graphic-p))
 
 ;; Remove scroll bar
 (scroll-bar-mode -1)
@@ -307,6 +254,7 @@
                 shell-mode-hook
                 treemacs-mode-hook
                 eshell-mode-hook
+                vterm-mode-hook
                 cider-repl-mode-hook
                 cider-stacktrace-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
@@ -331,8 +279,11 @@
 (setq visible-bell t)
 (setq ring-bell-function 'ignore)
 
-;; Remove blinking cursor
-(blink-cursor-mode -1)
+;; Set blinking cursor
+(blink-cursor-mode +1)
+
+;; Set the cursor type as a thin vertical bar
+(setq-default cursor-type 'bar)
 
 ;; Enable moving to buffers using arrow keys
 ;; [TODO]: Find a set of keybindings that do not
@@ -349,37 +300,55 @@
   :hook
   (prog-mode . rainbow-delimiters-mode))
 
+(defvar favorite-themes '(doom-city-lights
+                          doom-dark+
+                          doom-dracula
+                          doom-material
+                          doom-monokai-pro
+                          doom-nord
+                          doom-one
+                          doom-opera))
+
+(defun my/get-random-theme ()
+  (nth (random (length favorite-themes)) favorite-themes))
+
 ;; Doom-themes for the win!
-;; (use-package doom-themes
-;;   :config
-;;   (load-theme 'doom-one t)
-;;   Add padding to the modeline
-;;   (setq doom-themes-padded-modeline t))
+(use-package doom-themes
+  :config
+  ;; (load-theme 'doom-opera-light t)
+  (load-theme (my/get-random-theme) t))
 
-;; Uncomment to enable modus-themes
-(setq modus-themes-mode-line '(accented borderless)
-      modus-themes-region '(bg-only accented)
-      modus-themes-italic-constructs t
-      modus-themes-paren-match '(intense)
-      modus-themes-subtle-line-numbers t
-      modus-themes-org-blocks 'gray-background)
-
-(defun my/modus-themes-toggle ()
-  (interactive)
-  (modus-themes-toggle)
-  (my/set-modeline-font-size))
-
-;; Switch between dark and light themes
-(global-set-key (kbd "<f5>") 'my/modus-themes-toggle)
-
-(load-theme 'modus-operandi t)
+;; (use-package modus-themes
+;;   :bind
+;;   ("<f5>" . modus-themes-toggle)
+;;   :init
+;;   (setq modus-themes-mode-line '(accented borderless)
+;;         modus-themes-region '(bg-only accented)
+;;         modus-themes-italic-constructs t
+;;         modus-themes-paren-match '(intense)
+;;         modus-themes-subtle-line-numbers t
+;;         modus-themes-org-blocks 'gray-background)
+;;   (load-theme 'modus-vivendi t))
 
 ;; Better modeline
 (use-package doom-modeline
   :init
   (doom-modeline-mode 1)
+  (setq doom-modeline-height 32))
+
+(use-package centaur-tabs
+  :demand
+  :init
+  (setq centaur-tabs-style "bar")
+  (setq centaur-tabs-set-icons t)
+  (setq centaur-tabs-set-bar 'over)
+  (setq centaur-tabs-set-modified-marker t)
   :config
-  (my/set-modeline-font-size))
+  (centaur-tabs-mode 1)
+  (centaur-tabs-headline-match)
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward))
 
 ;; Dealing with pairs (parenthesis, brackets, etc)
 (use-package smartparens
@@ -406,9 +375,7 @@
   :hook
   (lsp-mode . lsp-enable-which-key-integration)
   :init
-  (setenv "LSP_USE_PLISTS" "true")
   (setq lsp-keymap-prefix "C-c l")
-  (setq lsp-use-plists t)
   :config
   (setq lsp-log-io nil)
   (setq lsp-restart 'auto-restart)
@@ -423,8 +390,7 @@
   (setq lsp-enable-snippet nil)
   (setq lsp-lens-enable nil)
   (setq lsp-enable-indentation nil)
-  ;; Use corfu as completion
-  (setq lsp-completion-provider :none)
+  (setq lsp-completion-provider :none) ;; Use corfu as completion
   (setq lsp-rust-analyzer-cargo-watch-command "clippy"))
 
 ;; LSP + Treemacs integration
@@ -494,6 +460,71 @@
 (use-package yaml-mode
   :defer t
   :commands yaml-mode)
+
+(defun my/org-mode-setup ()
+  "Custom 'org-mode' setup."
+  (org-indent-mode)
+  (visual-line-mode 1))
+
+(use-package org
+  :pin org
+
+  :commands (org-capture org-agenda)
+
+  :hook
+  (org-mode . my/org-mode-setup)
+
+  :config
+  (setq org-ellipsis " ▾")
+  (setq org-hide-emphasis-markers t)
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "REVIEW(r)" "|" "DONE(d!)")))
+
+  (setq org-agenda-start-with-log-mode t)
+
+  (setq org-log-done 'time)
+
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  (require 'org-tempo)
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+
+  (org-babel-do-load-languages 'org-babel-load-languages
+                               '((emacs-lisp . t)))
+
+  (setq org-confirm-babel-evaluate nil)
+
+  (defun my/org-babel-tangle-config ()
+    (when (string-equal (buffer-file-name) my/config-file)
+      ;; Dynamic scoping to the rescue
+      (let ((org-confirm-babel-evaluate nil))
+        (org-babel-tangle))))
+
+  ;; Automatically tangle our Emacs.org config file when we save it
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (add-hook 'after-save-hook #'my/org-babel-tangle-config))))
+
+(use-package org-bullets
+  :after org
+  :hook
+  (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(use-package org-roam
+  :bind
+  (("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n f" . org-roam-node-find)
+   ("C-c n i" . org-roam-node-insert))
+  :config
+  (setq org-roam-directory "~/dev/org-roam")
+  (setq org-roam-completion-everywhere t)
+  (org-roam-setup))
 
 ;; Start emacs server to enable emacsclient
 (if (and (fboundp 'server-running-p)
