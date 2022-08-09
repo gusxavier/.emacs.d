34c1d776ecd01e25377fd1ee308d5d41b8e06837
Gustavo Xavier <gustavo.xavier@180s.com.br>
2022-08-09 20:50:55 -0300

    Add all-the-icons-completion package

--- init.el
+++ init.el
@@ -281,6 +281,12 @@
 (use-package all-the-icons
   :if (display-graphic-p))
 
+(use-package all-the-icons-completion
+  :after (marginalia all-the-icons)
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
+
 ;; Remove scroll bar
 (scroll-bar-mode -1)
 
